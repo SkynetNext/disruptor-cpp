d329b49b2e41e2de336eb7a6290a650a91a19414
justinzhu@home <zhuguangwen@zealshow.com>
2025-12-29 21:35:50 +0800

    lint

--- include/disruptor/EventProcessor.h
+++ include/disruptor/EventProcessor.h
@@ -1,8 +1,7 @@
 #pragma once
 // 1:1 port of com.lmax.disruptor.EventProcessor
-// Source: reference/disruptor/src/main/java/com/lmax/disruptor/EventProcessor.java
-
-#include <functional>
+// Source:
+// reference/disruptor/src/main/java/com/lmax/disruptor/EventProcessor.java
 
 namespace disruptor {
 
@@ -15,7 +14,7 @@ public:
   // Java extends Runnable; we model it as a run() method.
   virtual void run() = 0;
 
-  virtual Sequence& getSequence() = 0;
+  virtual Sequence &getSequence() = 0;
   virtual void halt() = 0;
   virtual bool isRunning() = 0;
 };
