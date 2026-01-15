35207b29e8fca35ad26661c0ad784141ed639467
zhuguangwen <zhuguangwen@zealshow.com>
2026-01-15 10:13:58 +0800

    Revert "objects pool"
    This reverts commit bd5fabb3aacacad4506deb7eda40dc6d4122f1a9.

--- tests/disruptor/DisruptorStressTest.cpp
+++ tests/disruptor/DisruptorStressTest.cpp
@@ -44,11 +44,9 @@ TEST(DisruptorStressTest, shouldHandleLotsOfThreads_smoke) {
   using WS = disruptor::BusySpinWaitStrategy;
   auto& tf = disruptor::util::DaemonThreadFactory::INSTANCE();
   WS ws;
-  
-  // ⚠️ FIX: Move fatal BEFORE disruptor to ensure it outlives the disruptor and all threads
-  disruptor::FatalExceptionHandler<TestEvent> fatal;
-  
   disruptor::dsl::Disruptor<TestEvent, disruptor::dsl::ProducerType::MULTI, WS> d(TestEvent::FACTORY, 1 << 12, tf, ws);
+
+  disruptor::FatalExceptionHandler<TestEvent> fatal;
   d.setDefaultExceptionHandler(fatal);
 
   constexpr int threads = 2;
@@ -110,6 +108,4 @@ TEST(DisruptorStressTest, shouldHandleLotsOfThreads_smoke) {
     EXPECT_GT(h.messagesSeen, 0);
     EXPECT_EQ(h.failureCount, 0);
   }
-  
-  // ⚠️ FIX: fatal will be destructed AFTER d, handlers, and all threads have finished
 }
